7ef6175984ad6ce8dbd4d6af9f5189bfac0ced6d
Mateusz <mat.gregorowicz@gmail.com>
2014-12-04 20:06:08 +0100

    widok rejestracji + nowe logowanie

--- Galeria/urls.py
+++ Galeria/urls.py
@@ -3,6 +3,7 @@ from Galeria.views import *
 
 urlpatterns = patterns('',
                        url(r'^$', log_in),
+                       url(r'log_in/$', log_in),
                        url(r'register/$', register),
                        url(r'albums/$', albums_view)
 )
