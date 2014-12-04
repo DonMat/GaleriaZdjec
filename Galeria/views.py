6924540b4fd0013c6d3a367ed7480df833978aa2
Mateusz <mat.gregorowicz@gmail.com>
2014-12-04 19:53:54 +0100

    test

--- Galeria/views.py
+++ Galeria/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.template import loader, Context
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.core.context_processors import csrf
+from datetime import datetime
+from django.http import HttpResponseNotFound
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+
+def albums_view(request):
+    return render_to_response('albums.html')
+
+def log_in(request):
+    return render_to_response('log_in.html')
+
+def register(request):
+    return render_to_response('register.html')
