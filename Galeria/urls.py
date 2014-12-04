65a0a7ad3cd47cf83c38bb1d373d5d76b99a48b7
Mateusz <mat.gregorowicz@gmail.com>
2014-12-04 19:57:53 +0100

    2 widoki do rejestracji + logowania i template pod widok albumów

---
+++ Galeria/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import *
+from Galeria.views import *
+
+urlpatterns = patterns('',
+                       url(r'^$', log_in),
+                       url(r'register/$', register),
+                       url(r'albums/$', albums_view)
+)
